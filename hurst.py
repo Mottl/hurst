729b8deefe6ee292fd614db896803264c3bc284d
Dmitry Mottl <dmitry.mottl@gmail.com>
2018-06-05 18:47:30 +0200

    computeHc() now return data for plotting

--- hurst.py
+++ hurst.py
@@ -9,8 +9,13 @@ https://en.wikipedia.org/wiki/Rescaled_range
 
 __version__ = '0.0.2'
 
-import numpy as np
+import sys
 import math
+import numpy as np
+try:
+    import pandas as pd
+except:
+    pass
 
 def _to_inc(x):
     _x = x if type(x) == np.ndarray else np.array(x)
@@ -33,13 +38,16 @@ def get_RS(series):
         (Time-)series
     """
 
+
     incs = _to_inc(series)
 
     R = max(series)-min(series) # range
     S = np.std(incs) # standard deviation
-    return R/S
+    if R == 0 or S == 0:
+        return 0  # return 0 to skip this interval due undefined R/S
+    return R / S
 
-def compute_Hc(series):
+def compute_Hc(series, min_window=10):
     """
     Compute H (Hurst exponent) and C according to Hurst equiation:
     E(R/S) = c * T^H
@@ -51,14 +59,40 @@ def compute_Hc(series):
 
     series : array-like
         (Time-)series
+
+    min_window : int, default 10
+        the minimal window size for R/S calculation
+
+    Returns tuple of
+        H, c and data
+        where H and c — parameters or Hurst equiation
+        and data is a list of 2 lists: time intervals and R/S-values for correspoding time interval
+        for further plotting log(data[0]) on X and log(data[1]) on Y
     """
 
     if len(series)<100:
         raise ValueError("Series length must be greater or equal to 100")
 
+    ndarray_likes = [np.ndarray]
+    if "pandas.core.series" in sys.modules.keys():
+        ndarray_likes.append(pd.core.series.Series)
+
+    # convert series to numpy array if series is not numpy array or pandas Series
+    if type(series) not in ndarray_likes:
+        series = np.array(series)
+
+    if "pandas.core.series" in sys.modules.keys() and type(series) == pd.core.series.Series:
+        if series.isnull().values.any():
+            raise ValueError("Series contains NaNs")    
+    elif np.isnan(np.min(series)):
+        raise ValueError("Series contains NaNs")
+
+    err = np.geterr()
+    np.seterr(all='raise')
+
     window_sizes = list(map(
         lambda x: int(10**x),
-        np.arange(1., math.log10(len(series)-1), 0.25)))
+        np.arange(math.log10(min_window), math.log10(len(series)-1), 0.25)))
     window_sizes.append(len(series))
 
     RS = []
@@ -67,13 +101,17 @@ def compute_Hc(series):
         for start in range(0, len(series), w):
             if (start+w)>len(series):
                 break
-            rs.append(get_RS(series[start:start+w]))
-        RS.append(np.average(rs))
+            _ = get_RS(series[start:start+w])
+            if _ != 0:
+                rs.append(_)
+        RS.append(np.mean(rs))
 
     A = np.vstack([np.log10(window_sizes), np.ones(len(RS))]).T
     H, c = np.linalg.lstsq(A, np.log10(RS), rcond=-1)[0]
+    np.seterr(**err)
+
     c = 10**c
-    return H, c
+    return H, c, [window_sizes, RS]
 
 if __name__ == '__main__':
     prices = [100.]
@@ -81,5 +119,5 @@ if __name__ == '__main__':
     for pct_change in series:
         prices.append(prices[-1] * (1.+pct_change/100))
 
-    H, c = compute_Hc(np.array(prices))
+    H, c, data = compute_Hc(np.array(prices))
     print("H={:.4f}, c={:.4f}".format(H,c))
